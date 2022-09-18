50cfa37e40501ac02fe48b6d9f4b16378f1be566
vincentwtln <108678688+vincentwtln@users.noreply.github.com>
2022-09-18 14:10:12 +0000

    Deploying to gh-pages from  @ 627ab5a5b0fc32cb82d870c14b4ce423e2a99305 🚀

---
+++ Shared/StocksNavMenu.razor.js
@@ -0,0 +1,8 @@
+﻿function openBurger() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
