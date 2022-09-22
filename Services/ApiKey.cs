2f0f167e0bf8660bc6a320b7edd46bf1bf5d1398
Vincent Wittlin <vincent.wittlin@bdo.ch>
2022-09-22 13:59:22 +0200

    API Refresh

--- Services/ApiKey.cs
+++ Services/ApiKey.cs
@@ -3,7 +3,7 @@
     public class ApiKey
     {
         /*public string myApiKey = "99f23a45d3a2b907be4f10a42ff67cc2";*/
-        public string myApiKey = "e58cc3bef2aa85bdbfb837d32505c2a2";
-        /* public string myApiKey = "d40b493e9608d3bfbc9e9a39084f54d3";*/
+        /*public string myApiKey = "e58cc3bef2aa85bdbfb837d32505c2a2";*/
+        public string myApiKey = "d40b493e9608d3bfbc9e9a39084f54d3";
     }
 }
